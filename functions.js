56cd6158e1984d3845a972c36eb7a34c027070a3
Pedro Van <vanderock1@gmail.com>
2015-11-06 05:08:16 +0000

    ready for demo

--- functions.js
+++ functions.js
@@ -39,7 +39,6 @@ function addSale(){
 	var n=$("#pname").html();
 	var pr=$("#price").html();
 	var q=$("#qty").val();
-	//alert(q);
 	var strUrl = "http://cs.ashesi.edu.gh/~csashesi/class2016/agatha-maison/MWC/response.php?cmd=7&sid="+s+
 		"&pname="+n+"&price="+pr+"&qty="+q+"&pid="+prodId;
 	// var strUrl = "response.php?cmd=7&sid="+s+"&pname="+n+"&price="+pr+"&qty="+q+"&pid="+prodId;
@@ -48,7 +47,9 @@ function addSale(){
 		alert("successfully saved.");
 	}
 	receipt();
-	
+	$("#pname").val("");
+	$("#price").val("");
+	$("#qty").val("");
 }
 
 function addTrans(){
@@ -72,24 +73,22 @@ function genId(){
 		setDate();
 }
 
-		function scan(){
-			
-			cordova.plugins.barcodeScanner.scan(
-	      		function (result) {
-	      			prodId = result.text;	                
-	                var strUrl = "http://cs.ashesi.edu.gh/~csashesi/class2016/agatha-maison/MWC/response.php?cmd=4&pid="+prodId;
-	                var objResult=sendRequest(strUrl);
-					if(objResult.result==1){
-						// alert("successful.");
-						$("#pname").val(objResult.product[0]['pname']);
-						$("#price").val(objResult.product[0]['price']);						
-					}
-	      		}, 
-	      		function (error) {
-	          		alert("Scanning failed: " + error);
-	      		}
-   			);
+function scan(){
+	cordova.plugins.barcodeScanner.scan(
+	 	function (result) {
+			prodId = result.text;	                
+			var strUrl = "http://cs.ashesi.edu.gh/~csashesi/class2016/agatha-maison/MWC/response.php?cmd=4&pid="+prodId;
+			var objResult=sendRequest(strUrl);
+			if(objResult.result==1){
+				$("#pname").val(objResult.product[0]['pname']);
+				$("#price").val(objResult.product[0]['price']);						
+			}
+		}, 
+		function (error) {
+	  		alert("Scanning failed: " + error);
 		}
+   	);
+}
 
 function receipt(){
 	var id=transid;
@@ -98,7 +97,6 @@ function receipt(){
 	var objResult=sendRequest(strUrl);
 	if(objResult.result==1){
 		var list = "";
-		// var popupWindow = "";
 		var total=0;
 		for(var i = 0; i < objResult.sale.length; i++){
 			list += "<li class='ui-listview ui-listview-inset ui-corner-all ui-shadow' data-theme='b'>";
@@ -106,21 +104,16 @@ function receipt(){
 			list += "<p>"+objResult.sale[i].price+"</p>";
 			list += "<p>"+objResult.sale[i].qty+"</p>";
 			list += "<div onclick='getProductId("+objResult.sale[i].sid+")' data-rel='popup' class='ui-shadow ui-btn-right ui-corner-all ui-btn-inline ui-icon-bars ui-btn-icon-notext ui-btn-b ui-mini'>icon only button</div>";
-			//list += "<a href='#purchase' data-role='button' onclick='edit("+objResult.sale[i].pname+")' class='ui-shadow ui-btn-right ui-corner-all ui-btn-inline ui-icon-delete ui-btn-icon-notext ui-btn-b ui-mini'>icon only button</a>";
-			// list += "<a href='#purchase' data-rel='popup' data-position-to='window' data-transition='pop' aria-haspopup='true' aria-owns='purchase' aria-expanded='false' class='ui-btn ui-btn-icon-notext ui-icon-gear ui-btn-a' title='Purchase album'></a>";
+			
 			total += (objResult.sale[i]['price'] * objResult.sale[i]['qty']);
 		}
 		$("#rec").html(list);
-		//var strUrl = "http://cs.ashesi.edu.gh/~csashesi/class2016/agatha-maison/MWC/response.php?cmd=10&sid="+id+"&total="+total;
+		var strUrl = "http://cs.ashesi.edu.gh/~csashesi/class2016/agatha-maison/MWC/response.php?cmd=10&sid="+id+"&total="+total;
 		var strUrl = "response.php?cmd=10&sid="+id+"&total="+total;
 		var objResult=sendRequest(strUrl);
 		if(objResult.result==1){
 			$("#total").html("<b>TOTAL: </b>GHC "+total);
 		}
-
-// reduce qty
-
-
 
 	}
 	var dis="";
@@ -132,7 +125,6 @@ function getProductId(id){
 	var text="";
 	// text += "<h4>Permanently Delete Sale?</h4>";
 	text += "<a href='#' onclick='deleteSale("+id+")' data-role='button' data-rel='back' class='ui-shadow ui-btn ui-corner-all ui-icon-delete ui-btn-icon-left ui-btn-inline ui-mini'>Delete</a>";
-	// text += "<a href='#' onclick='editSale("+id+")' data-role='button' data-rel='back' class='ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-edit ui-btn-icon-left ui-mini'>Edit</a>";
 	text += "<a href='#' data-role='button' data-rel='back' class='ui-shadow ui-btn ui-corner-all ui-btn-inline ui-mini'>Cancel</a>";
 	$('#purchase').html(text);
 	$('#purchase').popup("open", { theme: "b" } );
